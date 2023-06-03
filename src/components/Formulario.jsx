b711d49ca472556accf80b3b7ec9c6973daea869
Nicolas Powell <nicopowell30@gmail.com>
2023-06-03 17:17:02 -0300

    agregué la consulta a la api y que se muestren las noticias en pantalla

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,28 +1,32 @@
-import React from "react";
+import { useState } from "react";
 import { Form, Col, Row } from "react-bootstrap";
 
-const Formulario = () => {
+const Formulario = ({consultarAPI}) => {
+  const [categoria, setCategoria] = useState("");
+
+ const handleOptionChange = (event) => {
+    const categoriaSeleccionada = event.target.value;
+    setCategoria(categoriaSeleccionada); // Actualiza el estado con la categoría seleccionada
+    if (categoriaSeleccionada === "Opciones") {
+      return; // No hace nada si se selecciona "Opciones"
+    }
+    consultarAPI(categoriaSeleccionada); // Llama a la función consultarAPI con el valor de la categoría
+  };
+
   return (
     <Form>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+      <Form.Group as={Row} className="mb-3" controlId="inputFormulario">
         <Form.Label column sm="2">
           Buscar por categoria
         </Form.Label>
         <Col sm="10">
-          <Form.Select aria-label="Default select example">
+          <Form.Select onChange={handleOptionChange} value={categoria}>
             <option>Opciones</option>
             <option value="science">Ciencia</option>
-            <option value="food">Comida</option>
             <option value="sports">Deportes</option>
             <option value="entertainment">Entretenimiento</option>
-            <option value="environment">Medio ambiente</option>
-            <option value="world">Mundo</option>
-            <option value="business">Negocios</option>
             <option value="top">Noticias destacadas</option>
-            <option value="politics">Política</option>
-            <option value="health">Salud</option>
             <option value="technology">Tecnología</option>
-            <option value="tourism">Turismo</option>
           </Form.Select>
         </Col>
       </Form.Group>
--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Card, Col, Button } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 
-const Noticia = () => {
+const Noticia = ({noticia}) => {
   return (
     <Col sm={12} md={6} lg={4}>
-      <Card>
-        <Card.Img variant="top" src="https://images.pagina12.com.ar/styles/focal_3_2_960x640/public/2023-06/732609-captura-20de-20pantalla-202023-06-03-20150158.jpg?h=06f6671c&itok=hFGw1vwl" />
+      <Card className="cardContainer">
+        <Card.Img variant="top" src={noticia.image_url} />
         <Card.Body>
-          <h6 className="pb-2">Diario Z</h6>
-          <Card.Title>Titulo noticia</Card.Title>
+          <h6 className="pb-2">{noticia.source_id}</h6>
+          <Card.Title>{noticia.title}</Card.Title>
           <Card.Text className="text-secondary">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, quisquam ea sed iste, nemo aliquid eveniet incidunt cupiditate nostrum expedita veniam tenetur aut id, adipisci cumque magni aspernatur ipsa nulla.
+            {noticia.description}
           </Card.Text>
           <hr />
-          <Button variant="primary" className='w-100 py-2'>Ver noticias completas</Button>
+          <a className='btn btn-primary w-100 py-2' href={noticia.link}>Ver noticias completas</a>
         </Card.Body>
       </Card>
     </Col>
