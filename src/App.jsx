8f7b1434aaa046a1468cd04f44d69c60568be7a4
Nicolas Powell <nicopowell30@gmail.com>
2023-06-03 15:57:31 -0300

    cree el maquetado del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from 'react-bootstrap';
 import Titulo from './components/Titulo'
+import Formulario from './components/formulario';
 
 function App() {
   return (
     <>
       <Titulo></Titulo>
       <hr />
-      <Container></Container>
+      <Container className='border p-4'>
+        <Formulario></Formulario>
+      </Container>
     </>
   )
 }
--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Titulo = () => {
   return (
-    <h1 className='text-center display-4 mt-2'>Noticias</h1>
+    <h1 className='text-center display-4 mt-4'>Noticias</h1>
   );
 };
 
