7eb038b46cc0ce5f27cafd9ecd2571ed2ef2f6c4
Nicolas Powell <nicopowell30@gmail.com>
2023-06-03 15:29:20 -0300

    creé el componente titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from 'react-bootstrap';
+import Titulo from './components/Titulo'
 
 function App() {
   return (
     <>
+      <Titulo></Titulo>
+      <hr />
+      <Container></Container>
     </>
   )
 }
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Titulo = () => {
+  return (
+    <h1 className='text-center display-4 mt-2'>Noticias</h1>
+  );
+};
+
+export default Titulo;
